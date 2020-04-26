b8c00f835914616e170c6b035c6c305ef7cde69e
herbertsds <herbertsds.ufrj@gmail.com>
2020-04-26 00:53:50 -0300

    Created input commands using yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,56 @@
 const chalk = require('chalk')
+//Importing argv as separated variable to facilite
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
+//Making the logs more easy
 const log = console.log
 
-const command = process.argv[2]
+//Customize yargs version
+yargs.version('1.1.0')
 
-log(process.argv)
 
-if (command === 'add'){
-    log("Adding note!")
-}else if (command === 'remove'){
-    log('Removing note!')
-}
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        log('Adding a new note', argv)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        log('Removing the note')
+    }
+
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function(){
+        log('Listing all notes')
+    }
+})
+
+// Create the read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        log('Reading a note')
+    }
+})
+
+yargs.parse()

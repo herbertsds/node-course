1964a16f4050db0385c63eab117b8fc4914bfea2
herbertsds <herbertsds.ufrj@gmail.com>
2020-04-26 12:02:22 -0300

    Read data from file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,17 +1,23 @@
 const log = console.log
 const fs = require('fs')
 
-const book = {
-    title: 'Ego is the enemy',
-    author: 'Ryan Holiday'
-}
+// const book = {
+//     title: 'Ego is the enemy',
+//     author: 'Ryan Holiday'
+// }
 
-// Convert object to JSON
-const bookJSON = JSON.stringify(book)
-log(bookJSON)
+// // Convert object to JSON
+// const bookJSON = JSON.stringify(book)
+// log(bookJSON)
 
-// Convert JSON to object
-const parsedData = JSON.parse(bookJSON)
-log(parsedData.author)
+// // Convert JSON to object
+// const parsedData = JSON.parse(bookJSON)
+// log(parsedData.author)
 
-fs.writeFileSync('1-json.json', bookJSON)
+// fs.writeFileSync('1-json.json', bookJSON)
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const data = JSON.parse(dataJSON)
+
+log(data.title)

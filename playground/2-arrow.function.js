bfd3d9f792abb7dca5d972b2dad1edb8ad1665c7
herbertsds <herbertsds.ufrj@gmail.com>
2020-04-26 16:07:10 -0300

    Arrow Functions, Methods and Objects

--- playground/2-arrow.function.js
+++ playground/2-arrow.function.js
@@ -2,6 +2,20 @@
 //  return x*x
 // }
 
-const square = (x) => x*x 
+// const square = (x) => x*x 
 
-console.log(square(3))
+// console.log(square(3))
+
+const event = {
+    name: 'Birthday Party',
+    guestList: ['Andrew', 'Jen', 'Mike'],
+    printGuestList(){
+        console.log(`Guest list for ${this.name}`)
+
+        this.guestList.forEach((guest) => {
+            console.log(`${guest} is attending ${this.name}`)
+        })
+    }
+}
+
+event.printGuestList()

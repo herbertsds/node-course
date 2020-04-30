f746d144f1faf9456d5d9a483d43976bfaa6ff50
herbertsds <herbertsds.ufrj@gmail.com>
2020-04-30 20:35:00 -0300

    Making API call to mapbox

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,26 @@
 const access_key = require('./access_key')
 const request = require('postman-request')
 
-const url = `http://api.weatherstack.com/current?access_key=${access_key}&query=37.8267,-122.4233`
+const weatherstack_url = `http://api.weatherstack.com/current?access_key=${access_key.weatherstack}&query=37.8267,-122.4233`
 
-request({ url }, (error, response) => {
-    const data = JSON.parse(response.body)
+const mapbox_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${access_key.mapbox}&limit=1`
 
-    console.log(data.current)
+request({ url: weatherstack_url, json: true }, (error, response) => {
+
+    const {temperature, feelslike, weather_descriptions} = response.body.current
+
+    const message = `${weather_descriptions}. It is currently ${temperature} degrees out. It feels like ${feelslike} degress out.`
+
+    console.log(message)
+
+})
+
+request({ url: mapbox_url, json: true }, (error, response) => {
+
+    const { center } = response.body.features[0]
+
+    const latitude = center[1]
+    const longitude = center[0]
+
+    console.log(latitude, longitude)
 })

416eac494f7120dd842fb5b93b041f45a0f28b08
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-02 15:38:08 -0300

    Serving dinamic content with hbs. Partials and views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,43 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-const pubDirectoryPath = path.join(__dirname, '../public')
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-app.use(express.static(pubDirectoryPath))
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Herbert Salazar'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Herbert Salazar'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'Help me please!',
+        title: 'Help',
+        name: 'Herbert Salazar'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({

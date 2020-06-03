2218a4928710d2a42b68953fdd788f2c347e738f
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-03 12:41:45 -0300

    Getting params from browser using query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,9 +40,30 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
     res.send({
         forecast:'Light Rain',
-        Location: 'Belford Roxo'
+        Location: 'Belford Roxo',
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+
+    if(!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    res.send({
+        products: []
     })
 })
 

a4978ecab347ebea129e5856a2226449f1aef3d4
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-03 15:01:28 -0300

    Fetching geocode data

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,18 @@
 console.log('Client side javascript file is loaded!')
+
+fetch('/weather?address=boston').then((response) => {
+    response.json().then( (data) => {
+        if(data.error){
+            console.error(data.error)
+        }else{
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })

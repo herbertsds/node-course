02b6e0c78a4fab3053bf9d4216fef8f40009a3e2
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-03 15:47:49 -0300

    Showing forecast to users

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,22 +1,30 @@
 console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
-
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     
     const location = search.value
+    
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    messageThree.textContent = ''
 
     fetch(`/weather?address=${encodeURIComponent(location)}`).then((response) => {
         response.json().then( (data) => {
+
             if(data.error){
-                console.error(data.error)
+                
+                messageOne.textContent = `Error: ${data.error}`
             }else{
-                console.log(data.address)
-                console.log(data.location)
-                console.log(data.forecast)
+                messageOne.textContent = `Searched for: ${data.address}`
+                messageTwo.textContent = `Found: ${data.location}`
+                messageThree.textContent = `Forecast: ${data.forecast}`
             }
         })
     })

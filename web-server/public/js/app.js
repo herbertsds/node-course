40937c3a8470a861a447440537d5485cce82cb89
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-03 15:22:58 -0300

    Fetching weather after submit

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,18 +1,23 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('/weather?address=boston').then((response) => {
-    response.json().then( (data) => {
-        if(data.error){
-            console.error(data.error)
-        }else{
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+const weatherForm = document.querySelector('form')
+
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const location = search.value
+
+    fetch(`/weather?address=${encodeURIComponent(location)}`).then((response) => {
+        response.json().then( (data) => {
+            if(data.error){
+                console.error(data.error)
+            }else{
+                console.log(data.address)
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
 })
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })

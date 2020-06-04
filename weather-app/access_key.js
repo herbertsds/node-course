df413f00a11ce81cbd16b9b1b65f50045392373e
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-04 16:31:43 -0300

    Including access_key files to Heroku

---
+++ weather-app/access_key.js
@@ -0,0 +1,7 @@
+const weatherstack = '77443014caba79acdc02bee1a4dd6c42'
+const mapbox = 'pk.eyJ1IjoiaGVyYmVydHNkcyIsImEiOiJjazlsdjg5ZmgwMjdqM2lyenpjaWV2dWozIn0.caxwhnsfoUwSoKT1_AEEYA'
+
+module.exports = {
+    weatherstack,
+    mapbox
+}

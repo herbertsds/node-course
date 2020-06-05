efbe0e611e08f2e932514e0a71d2af8b256ea7b7
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-05 11:34:28 -0300

    Adding new information in forecast

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -4,13 +4,13 @@ const access_key = require('../access_key')
 const forecast = (lat, long, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=${access_key.weatherstack}&query=${lat},${long}`
 
-    request({url, json: true}, (error, { body:{ error:responseError, current:{temperature, feelslike, weather_descriptions}={}, location:{name, country, region}={} } = {} } = {}) => {
+    request({url, json: true}, (error, { body:{ error:responseError, current:{temperature, feelslike, weather_descriptions, humidity}={}, location:{name, country, region}={} } = {} } = {}) => {
         if(error){
             callback('Unable to connect to weather service!')
         }else if(responseError){
             callback(responseError.info)
         }else{
-            callback(undefined, `${name}, ${region}, ${country}. ${weather_descriptions}. It is currently ${temperature} degrees out. It feels like ${feelslike} degress out.`)
+            callback(undefined, `${name}, ${region}, ${country}. ${weather_descriptions}. It is currently ${temperature} degrees out. It feels like ${feelslike} degress out. The humidity is ${humidity}%.`)
 
         }
 

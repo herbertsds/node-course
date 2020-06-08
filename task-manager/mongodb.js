195e2a8f525149f9d6fda1a958faa156db964e54
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-08 15:48:19 -0300

    Inserting many documents at the same time

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -13,15 +13,32 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
     
-    db.collection('users').insertOne({
-        name: 'Herbert',
-        age: 27
-    }, (error, result) => {
+    // db.collection('users').insertOne({
+    //     name: 'Herbert',
+    //     age: 27
+    // }, (error, result) => {
+    //     if(error){
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+
+    // })
+
+    db.collection('users').insertMany([
+        {
+            name: 'Jen',
+            age: 28
+        },{
+            name: 'Gunther',
+            age: 27
+        }
+
+    ], (error, result) => {
         if(error){
-            return console.log('Unable to insert user')
+            return console.log('Unable to insert documents')
         }
 
         console.log(result.ops)
-
     })
 })

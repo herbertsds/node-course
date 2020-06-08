3552845e03554d301735e9b8a8052d3f470bf39c
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-08 16:21:48 -0300

    Find one document

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,17 +1,9 @@
 // CRUD create read update delete
 
-// const mongodb = require('mongodb')
-// const MongoClient = mongodb.MongoClient
-// const ObjectID = mongodb.ObjectID
-
 const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
-
-const id = new ObjectID()
-console.log(id.id.length)
-console.log(id.toHexString())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>{
     if(error){
@@ -20,53 +12,13 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
     
-    // db.collection('users').insertOne({
-    //     name: 'Vikram',
-    //     age: 26
-    // }, (error, result) => {
-    //     if(error){
-    //         return console.log('Unable to insert user')
-    //     }
+    db.collection('users').findOne({ _id: new ObjectID('5ede8cb3b7d7ad268aada922')}, (error, user) =>{
+        if(error){
+            return console.log('Unable to fetch')
+        }
 
-    //     console.log(result.ops)
-
-    // })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Jen',
-    //         age: 28
-    //     },{
-    //         name: 'Gunther',
-    //         age: 27
-    //     }
-
-    // ], (error, result) => {
-    //     if(error){
-    //         return console.log('Unable to insert documents')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Do Node course',
-    //         completed: true
-    //     },{
-    //         description: 'Talk with her',
-    //         completed: false
-    //     },{
-    //         description: 'Do Master course',
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if (error){
-    //         return console.log('Unable to insert documents')
-    //     }
-
-    //     console.log(result.ops)
-    // })
+        console.log(user)
+    })
 
 
 })

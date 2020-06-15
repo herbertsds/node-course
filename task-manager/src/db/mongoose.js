ced2151f05b39b1a5fdf9f213963e9aa6b3d8f2c
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-15 15:49:54 -0300

    Setting up first API file

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,55 +6,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useUnifiedTopology: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value){
-            if(value.toLowerCase().includes('password'))
-                throw new Error('Password cannot contain "password"')
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-               throw new Error('Email is invalid') 
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value){
-            if(value < 0){
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     name: '    Herbert      ',
-//     email: 'MYEMAIL@MEAD.IO    ',
-//     password: '   mypasswor  '
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
 
 const Task = mongoose.model('Task', {
     description: {
@@ -67,14 +17,4 @@ const Task = mongoose.model('Task', {
         type: Boolean,
         default: false
     }
-})
-
-const task = new Task({
-    description: '   Learn Julia   ',
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error!', error)
 })

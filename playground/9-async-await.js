85837e53e34bde2b4e38282b261228b4c9dc53d7
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-19 12:39:33 -0300

    Using await

--- playground/9-async-await.js
+++ playground/9-async-await.js
@@ -1,6 +1,14 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve( a + b )
+        }, 2000)
+    })
+}
+
 const doWork = async () => {
-    throw new Error('Something went wrong')
-    return 'Herbert'
+    const sum = await add(1, 99)
+    return sum
 }
 
 

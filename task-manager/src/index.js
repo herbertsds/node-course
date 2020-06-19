1470e609f430ffc0cf0d1b374dabbce179061d51
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-19 16:42:23 -0300

    Deleting users and tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -76,6 +76,21 @@ app.patch('/users/:id', async (req, res) => {
 })
 
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if(!user){
+            return res.status(404).send()
+        }
+        
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -114,6 +129,42 @@ app.get('/tasks/:id', async (req, res) => {
         res.status(500).send()
     }
 
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperator = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperator){
+        res.status(400).send({error: 'Invalid updates!'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        
+        if(!task){
+            return res.status(404).send()
+        }
+        
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if(!task){
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.send(500).send()
+    }
 })
 
 app.listen(port, () => {

cb206faf2b5a47edee99df03509acc5f4642973d
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-29 15:31:57 -0300

    Logout all sessions

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -28,9 +28,23 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+// Logout one session
 router.post('/users/logout', auth, async (req, res) => {
     try {
         req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token)
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// Logout all sessions
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+
         await req.user.save()
 
         res.send()

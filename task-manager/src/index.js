ff9cc5d1588b47e940c55cfe8f1400dec336e8a9
herbertsds <herbertsds.ufrj@gmail.com>
2020-06-29 15:53:28 -0300

    Manipulating JSON stringify result

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -22,15 +22,13 @@ app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
 
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
-
+const pet = {
+    'name': 'Hal'
 }
 
-myFunction()
+pet.toJSON = function(){
+    console.log(this)
+    return this
+}
+
+console.log(JSON.stringify(pet))

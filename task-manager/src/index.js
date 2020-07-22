36083f6db9674e03c6dea1fa9d33f7e5b6aec399
herbertsds <herbertsds.ufrj@gmail.com>
2020-07-22 17:34:01 -0300

    Filtering and limiting file uploads

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,7 +8,21 @@ const port = process.env.PORT || 3000
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        
+        if(!file.originalname.match(/\.(doc|docx)$/)){
+            return cb(new Error('Please upload a Word document'))
+        }
+
+        cb(undefined, true)
+
+        // cb(new Error('File must be a PDF'))
+        // cb(undefined, true)
+    }
 })
 
 app.post('/upload', upload.single('upload'), (req, res) => {

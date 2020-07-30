a159d8b4b72d4416147e50dc38abc96adb508bf1
herbertsds <herbertsds.ufrj@gmail.com>
2020-07-30 17:14:52 -0300

    Runing test for a function

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,7 +1,9 @@
-test('Hello World!', () => {
-    
-})
+const { calculateTip } = require('../src/math')
 
-test('This should fail', () => {
-    throw new Error('Failure!')
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+
+    if(total !==13){
+        throw new Error(`Total tip should be 13. Got ${total}`)
+    }
 })
